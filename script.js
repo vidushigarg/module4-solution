3d0c3ee0aeea49a99d8abfde887144a466bf791b
Vidushi Garg <63226327+vidushigarg@users.noreply.github.com>
2020-06-05 21:34:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let name = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i = 0; i < name.length ; i++){
+  var firstLetter=name[i].charAt(0);
+  if(firstLetter.toLowerCase()=="j"){
+    byeSpeaker(name[i]);
+  } else{
+    helloSpeaker(name[i]);
+  }
+};
